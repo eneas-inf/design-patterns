2e8a026c391bca5ea67d94540c0b5ec34225e0c0
Eneas Infanger <eneas_infanger@sluz.ch>
2025-05-23 13:55:34 +0200

    Exercise 1: Polling to Observer

--- src/parking-lot.simulation.ts
+++ src/parking-lot.simulation.ts
@@ -1,9 +1,9 @@
-import { ParkingLot } from "./parking-lot.js";
+import { ParkingLot, ParkingLotEvent, ParkingLotSubscriber } from "./parking-lot.js";
+import * as console from "node:console";
 
 const maxFillIntervalMillis = 1000;
 const maxEmptyIntervalMillis = 2000;
 const initialFillPhaseMillis = 5000;
-const refreshDisplayIntervalMillis = 250;
 
 const sleep = (millis: number) => new Promise((r) => setTimeout(r, millis));
 
@@ -14,7 +14,6 @@ const fill = async (lot: ParkingLot) => {
   while (!lot.isFull()) {
     await sleep(rand(0, maxFillIntervalMillis));
     lot.enter();
-    console.log(`a car entered the lot ${lot.name}`);
   }
 };
 
@@ -22,22 +21,24 @@ const empty = async (lot: ParkingLot) => {
   while (!lot.isEmpty()) {
     await sleep(rand(0, maxEmptyIntervalMillis));
     lot.exit();
-    console.log(`a car left the lot ${lot.name}`);
   }
 };
 
-const display = async (lot: ParkingLot) => {
-  while (true) {
-    console.log(`${lot.name}: ${lot.occupied}/${lot.capacity} occupied`);
-    await sleep(refreshDisplayIntervalMillis);
+class Display implements ParkingLotSubscriber {
+  onCarChange(event: ParkingLotEvent): void {
+    if (event.type == 'enter') {
+      console.log(`A car entered the lot ${event.lotName}: ${event.occupied}/${event.capacity} occupied`);
+    } else {
+      console.log(`A car left the lot ${event.lotName}: ${event.occupied}/${event.capacity} occupied`);
+    }
   }
-};
+}
 
 const bahnhofParking = new ParkingLot("Bahnhof Parking", 100);
-const screen = display(bahnhofParking);
+const screen = new Display();
+bahnhofParking.subscribe(screen);
 const filler = fill(bahnhofParking);
 await sleep(initialFillPhaseMillis);
 const emptier = empty(bahnhofParking);
-await screen;
 await filler;
 await emptier;
--- src/parking-lot.ts
+++ src/parking-lot.ts
@@ -1,5 +1,6 @@
-export class ParkingLot {
+export class ParkingLot implements ParkingLotPublisher {
   public occupied: number = 0;
+  private subscribers: ParkingLotSubscriber[] = [];
 
   constructor(
     public name: string,
@@ -12,6 +13,7 @@ export class ParkingLot {
     } else {
       throw new Error(`the parking lot is full`);
     }
+    this.notifySubscribers('enter');
   }
 
   exit() {
@@ -20,6 +22,17 @@ export class ParkingLot {
     } else {
       throw new Error(`the parking lot is empty`);
     }
+    this.notifySubscribers('leave');
+  }
+
+  private notifySubscribers(type: ParkingLotEventType): void {
+    const event = {
+      lotName: this.name,
+      occupied: this.occupied,
+      capacity: this.capacity,
+      type,
+    };
+    this.subscribers.forEach(sub => sub.onCarChange(event));
   }
 
   isFull() {
@@ -29,4 +42,37 @@ export class ParkingLot {
   isEmpty() {
     return this.occupied == 0;
   }
+
+  subscribe(subscriber: ParkingLotSubscriber): void {
+    if (this.subscribers.includes(subscriber)) {
+      throw new Error("subscriber already subscribed")
+    }
+    this.subscribers.push(subscriber);
+  }
+
+  unsubscribe(subscriber: ParkingLotSubscriber): void {
+    const i = this.subscribers.indexOf(subscriber);
+    if (i < 0) {
+      throw new Error("subscriber not subscribed to this lot")
+    }
+    this.subscribers.splice(i, 1);
+  }
+}
+
+export interface ParkingLotPublisher {
+  subscribe(subscriber: ParkingLotSubscriber): void;
+  unsubscribe(subscriber: ParkingLotSubscriber): void;
+}
+
+export interface ParkingLotSubscriber {
+  onCarChange(event: ParkingLotEvent): void;
+}
+
+export type ParkingLotEventType = 'enter' | 'leave';
+
+export interface ParkingLotEvent {
+  readonly lotName: string,
+  readonly occupied: number,
+  readonly capacity: number,
+  readonly type: ParkingLotEventType
 }
